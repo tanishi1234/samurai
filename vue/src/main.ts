9ddfe5d242d03029aeff6334e5425548d8300fa2
ryohashioka <ryo.hashioka@gmail.com>
2022-04-26 23:56:11 +0900

    ts-ignore

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
-const apiBaseUrl: string = import.meta.env.VITE_API_BASE_URL ?? "/";
 // @ts-ignore heroku デプロイ時に発生するため無効
+const apiBaseUrl: string = import.meta.env.VITE_API_BASE_URL ?? "/";
 axios.defaults.baseURL = apiBaseUrl;
 
 createApp(App).mount('#app')
