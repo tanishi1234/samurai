569859e41f51bb05fc34c6024ae92dcb25d78249
ryohashioka <ryo.hashioka@gmail.com>
2022-04-26 23:46:46 +0900

    デバッグログ追加

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL ?? '/';
+console.log(import.meta.env)
+console.log(import.meta.env.VITE_API_BASE_URL)
+// axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL ?? '/';
 
 createApp(App).mount('#app')
