4bcf51ccbc8b41e91e16c9a459d6f7cb9e860dbf
ryohashioka <ryo.hashioka@gmail.com>
2022-04-26 23:39:31 +0900

    env で api の url を指定できるように

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
 createApp(App).mount('#app')
