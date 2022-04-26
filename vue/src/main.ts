2287159ef4f6e2f79a46f844f6dc2fa87e3d58a7
ryohashioka <ryo.hashioka@gmail.com>
2022-04-26 23:45:48 +0900

    初期値セット

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL ?? '/';
 
 createApp(App).mount('#app')
